d0ff3a71c04b000ae7818561f177d6785995a7ea
AniKhachatryan <ani_khachatryan@aybschool.am>
2023-02-10 21:36:53 +0400

    fix: setup.py fix sklearn required version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='ckd',
     version='0.1.0',
     packages=find_packages(include=['ckd', 'ckd.*']),
-    install_requires=['scikit-learn>=1.2.0', 'pandas>=1.5.3', 'fire>=0.5.0'],
+    install_requires=['scikit-learn==1.2.0', 'pandas>=1.5.3', 'fire>=0.5.0'],
     entry_points={'console_scripts': ['mybinary=cli:cli_func']},
     package_dir={'ckd': 'ckd'},
     package_data={'ckd': ['files/data/*', 'files/models/*']}
